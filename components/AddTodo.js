e2ccea1389fbb82aea20ae98441ffc755b16106a
asifkarim <asif.karim100@gmail.com>
2023-11-27 19:00:36 +0530

    Time to save the TODOs(mongodb)

---
+++ components/AddTodo.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import classes from "./AddTodo.module.css";
+
+const AddTodo = () => {
+  const [todo, setTodo] = useState("");
+
+  const submitHandler = async (event) => {
+    event.preventDefault();
+
+
+    const todoData = {
+      todo,
+    };
+
+
+    const response = await fetch("/api/new-todo", {
+      method: "POST",
+      body: JSON.stringify(todoData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await response.json();
+
+    console.log(data);
+  };
+  return (
+    <form className={classes.form} onSubmit={submitHandler}>
+      <input
+        type="text"
+        placeholder="Add Your Task"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+      />
+      <button>Add Task</button>
+    </form>
+  );
+};
+
+export default AddTodo;
